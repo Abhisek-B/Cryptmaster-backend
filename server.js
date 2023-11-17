3734de727f7805619d87279020be57a2c57a0bb9
azmisal <b20cs33@gectcr.ac.in>
2023-11-18 01:56:35 +0530

    production

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 dotenv.config();
 
 mongoose.connect(
-    process.env.DATABASE,
+    process.env.NODE_ENV_DATABASE,
   { useNewUrlParser: true, useUnifiedTopology: true }
 )
 .then(()=>console.log("Connected to Database"))
@@ -17,6 +17,8 @@ mongoose.connect(
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://cryptmaster-frontend.vercel.app/', 
+}));
 app.use("/app", routesUrls);
 app.listen(process.env.PORT || 5000, () => console.log("Server is running..."));
