10a4c32f12326f3f79213487a31fb7add159bf78
Abhisek-B <abhisekbiswaw819@gmail.com>
2023-11-18 02:14:23 +0530

    production

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ mongoose.connect(
 
 
 app.use(express.json());
-app.use(cors({
-  origin: 'https://cryptmaster-frontend.vercel.app/', 
-}));
+app.use(cors());
 app.use("/app", routesUrls);
 app.listen(process.env.PORT || 5000, () => console.log("Server is running..."));
